846c8cf278bdf70bddbde98a5baac6cb84fa8ce0
Mehmet CENGİZ <37652560+cengizmehmet@users.noreply.github.com>
2021-10-17 18:53:17 +0100

    Add files via upload

---
+++ BehavioralPatterns/TemplateMethodPattern/Fridge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern_Shopping
+{
+    public class Fridge : Shopping
+    {
+        public override void PaymentDetails()
+        {
+            paymentType = "INSTALMENT";
+        }
+
+        public override void Product()
+        {
+            productName = "Refrigator";
+        }
+    }
+}
---
+++ BehavioralPatterns/TemplateMethodPattern/TV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern_Shopping
+{
+    public class TV : Shopping
+    {
+        public override void PaymentDetails()
+        {
+            paymentType = "CASH!";
+        }
+
+        public override void Product()
+        {
+            productName = "Television";
+        }
+    }
+}
