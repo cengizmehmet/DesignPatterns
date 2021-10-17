59d335b0bad2fbdcc7ce23144ff74ea27ca0266d
Mehmet CENGİZ <37652560+cengizmehmet@users.noreply.github.com>
2021-10-17 18:33:54 +0100

    Add files via upload

---
+++ CreationalPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine(singleton.GetConnection());
+        }
+    }
+}
---
+++ CreationalPatterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private static string connectionString = "STH";
+        static private Singleton instance;
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public string GetConnection()
+        {
+            //TRY CONNECT VIA connectionString
+            string connectionState = "true";
+            if (connectionState == "true")
+            {
+                return "SUCCESS";
+            }
+            else
+            {
+                return "FAIL";
+            }
+        }
+    }
+}
