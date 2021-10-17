af6bf236ab575dae85cff94bab831b229691254d
Mehmet CENGİZ <37652560+cengizmehmet@users.noreply.github.com>
2021-10-17 18:46:19 +0100

    Add files via upload

---
+++ Behavioral/TemplateMethodPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern_Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shopping shopping1 = new TV();
+            shopping1.TemplateMethod();
+        }
+    }
+}
---
+++ Behavioral/TemplateMethodPattern/Shopping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern_Shopping
+{
+    public abstract class Shopping
+    {
+        protected string productName;
+        protected string paymentType;
+
+        public void TemplateMethod()
+        {
+            Start();
+            Product();
+            PaymentDetails();
+            Finish();
+        }
+
+        void Start()
+        {
+            Console.WriteLine("SHOPPING STARTED!");
+        }
+
+        void Finish()
+        {
+            Console.WriteLine("SHOPPING FINISHED!\n{0} is sold with {1}", productName, paymentType);
+        }
+
+        abstract public void Product();
+        abstract public void PaymentDetails();
+    }
+}
