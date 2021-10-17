c1b236bf9765a32cead52b26710669adf5e756bf
Mehmet CENGİZ <37652560+cengizmehmet@users.noreply.github.com>
2021-10-17 18:32:51 +0100

    Add files via upload

---
+++ CreationalPatterns/PrototypePattern/ConcreteGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_GameInfo
+{
+    public class ConcreteGame : IGamePrototype
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public ConcreteGame(int id, string name, string type)
+        {
+            ID = id;
+            Name = name;
+            Type = type;
+        }
+
+        public IGamePrototype Clone()
+        {
+            IGamePrototype game = new ConcreteGame(this.ID, this.Name, this.Type);
+            //return this.MemberwiseClone() as GamePrototype;
+            return game;
+        }
+    }
+}
---
+++ CreationalPatterns/PrototypePattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_GameInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGamePrototype game = new ConcreteGame(1, "HOI4", "Strategy");
+            IGamePrototype game1 = (ConcreteGame)game.Clone();
+            IGamePrototype game2 = game;
+            IGamePrototype game3 = new ConcreteGame(1, "HOI4", "Strategy");
+
+            if (game.Equals(game1))
+            {
+                Console.WriteLine("Same");
+            }
+            else
+            {
+                Console.WriteLine("Different");
+            }
+
+            if (game.Equals(game2))
+            {
+                Console.WriteLine("Same");
+            }
+            else
+            {
+                Console.WriteLine("Different");
+            }
+
+            if (game.Equals(game3))
+            {
+                Console.WriteLine("Same");
+            }
+            else
+            {
+                Console.WriteLine("Different");
+            }
+        }
+    }
+}
